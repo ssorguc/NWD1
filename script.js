a1f950dc7ebb3619c3770a0de523760d83c4cef8
ssorguc <sorgucselma@gmail.com>
2024-08-04 10:57:30 +0200

    Primjeri var, let, const, for, if

---
+++ script.js
@@ -0,0 +1,36 @@
+var godine = 20;
+
+const maxBrojProizvoda = 10;
+const pi = 3.14;
+
+let ime = "Selma";
+
+if (true) {
+    let prezime = "Sorguc";
+    godine = 21;
+    console.log(prezime, godine);
+}
+
+let x;
+console.log(x);
+
+let minBroj = null;
+if (maxBrojProizvoda < 10) {
+    minBroj = 5;
+} else {
+    minBroj = 10;
+}
+
+let niz = [10, 15, 20];
+console.log(niz[0]);
+
+const duzinaNiza = niz.length;
+console.log(duzinaNiza);
+
+let novac = 0;
+
+for (let i = 0; i < niz.length; i++) {
+    novac = niz[i] * 100;
+    console.log(niz[i])
+}
+
